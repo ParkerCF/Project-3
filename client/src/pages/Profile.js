0797b1f7d5f1431c661eb996635bf88683e547c3
ParkerCF <110678158+ParkerCF@users.noreply.github.com>
2023-03-27 19:59:08 -0400

    fixed product list visibility

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -58,12 +58,15 @@ const Profile = () => {
           isLoggedInUser={!profileId && true}
         />
       )}
+      
+  
+    {profileId ? null : <div className="my-4 p-4" style={{ border: '1px dotted #1a1a1a' }}>
+    <ProductForm profileId={profile._id} />
+    </div>}
 
-      <div className="my-4 p-4" style={{ border: '1px dotted #1a1a1a' }}>
-        <ProductForm profileId={profile._id} />
-      </div>
+      
     </div>
   );
-};
 
+};
 export default Profile;
